4e8d36d4d0ade45cd6111eeaca8e37e41b79ccba
Omar Herrera <135260857+OmarHSelles@users.noreply.github.com>
2026-06-12 11:49:13 +0200

    Tema 1 - Variables y tipos de datos

---
+++ 01-variables/ejemplos.js
@@ -0,0 +1,12 @@
+// Variables
+
+let nombre = "Omar";
+let edad = 36;
+let profesor = true;
+
+console.log(nombre);
+console.log(edad);
+console.log(profesor);
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof profesor);
---
+++ 01-variables/ejercicios.js
@@ -0,0 +1,18 @@
+let nombre = "Omar";
+let edad = 39;
+let ciudad = "Alicante";
+let esProgramador = true;
+let lenguaje = "JavaScript";
+
+console.log("Nombre", nombre);
+console.log("Edad", edad);
+console.log("Ciudad", ciudad);
+console.log("Es programador", esProgramador);
+console.log(
+  `Me llamo ${nombre}, tengo ${edad} años, vivo en la ciudad de ${ciudad} soy programado ${esProgramador} y el lenguaje que estoy aprendiendo es ${lenguaje}`,
+);
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof ciudad);
+console.log(typeof esProgramador);
